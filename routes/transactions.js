5d3eb58b242d3b7b742c8167d1bd397cf45811df
GabrielPIAUI <gabriel.r.machado6@aluno.senai.br>
2024-07-29 10:42:37 -0300

    Iniciando API pt. 3

---
+++ routes/transactions.js
@@ -0,0 +1,9 @@
+const express = require('express'); //Importar a framework express
+const router = express.Router(); //Criar um roteador
+const transactionsController = require('../controllers/transactionsController'); //Importar o controlador 
+
+//Definir uma rota para obter todas as transações
+router.get('./', transactionsController.getAllTransactions);
+
+//Exportando o roteador
+module.exports = router;
